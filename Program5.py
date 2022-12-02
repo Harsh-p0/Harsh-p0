cdadbed12e30ecd80a92160553121d68fa6ab97b
Harsh-p0 <100550856+Harsh-p0@users.noreply.github.com>
2022-12-02 19:00:31 +0530

    Update and rename Python5.py to Program5.py
    Prime or not Prime

---
+++ Program5.py
@@ -0,0 +1,13 @@
+number=int(input("ENTER ANY NUMBER"))
+if number>1:
+  For i in range(2,number):
+ if(number %1)==0
+  print (number,"IS NOT A PRIME NUMBER")
+  break
+else:
+print(number,"IS A PRIME NUMBER")
+else:
+print(number,"IS NOT A PRIME NUMBER")
+
+
+
