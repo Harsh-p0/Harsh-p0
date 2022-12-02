78a5bb0c5c3adae8ac8f3e029b3d811e6b8f5d46
Harsh-p0 <100550856+Harsh-p0@users.noreply.github.com>
2022-12-02 20:09:29 +0530

    Create Program9.py
    Simple interest

---
+++ Program9.py
@@ -0,0 +1,5 @@
+Amount=int(input("ENTER THE AMOUNT:"))
+Year=int(input("ENTER THE YEAR:"))
+Rate=float(input("ENTER THE RATE OF INTEREST:))
+Simple Interest=(Amount*Year*Rate)/100
+print("THE SIMPLE INTEREST IS:",Simple Interest)
